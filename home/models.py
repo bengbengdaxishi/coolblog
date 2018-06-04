c93fb62bcc19a105000a8e988b5fd1e9f1461e2b
bengbengdaxishi <874216399@qq.com>
2018-06-04 16:09:18 +0800

    first

---
+++ home/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50,verbose_name="分类名")
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50,verbose_name="标签名")
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=70,verbose_name="文章标题")
+    body = models.TextField(verbose_name="文章内容",default="")
+    created_time = models.DateTimeField(verbose_name="创建时间")
+    modified_time = models.DateTimeField(verbose_name="修改时间")
+    excerpt = models.CharField(max_length=200,blank=True,verbose_name="摘要")
+    category = models.ForeignKey(Category,verbose_name="分类")
+    tags = models.ManyToManyField(Tag,blank=True,verbose_name=u"标签")
+    author = models.ForeignKey(User,verbose_name="作者")
+    views = models.PositiveIntegerField(default=0,verbose_name="浏览量")
